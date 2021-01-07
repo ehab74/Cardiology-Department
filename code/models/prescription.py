7f8421efa5fe03b8debdd52dc8dbf796ff1f22b9
mmn420 <momen.rizk99@eng-st.cu.edu.eg>
2021-01-07 18:09:03 +0200

    added prescription model and resource

---
+++ code/models/prescription.py
@@ -0,0 +1,37 @@
+from db import db
+
+class PrescriptionModel(db.Model):
+    __tablename__ = 'Prescriptions'
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('Patients.id'))
+    patient = db.relationship("PatientModel")
+    text = db.Column(db.String(500))
+    patient_name = db.Column(db.String(80))
+    doctor_name = db.Column(db.String(80))
+
+    def __init__(self, patient_id: int, text: str, patient_name: str, doctor_name: str):
+        self.patient_id = patient_id
+        self.text = text
+        self.patient_name = patient_name
+        self.doctor_name = doctor_name
+    def json(self):
+        return{
+            "id": self.id,
+            "patient_id": self.patient_id,
+            "text": self.text,
+            "doctor_name": self.doctor_name,
+            "patient_name": self.patient_name
+
+        }
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+    
---
+++ code/resources/prescription.py
@@ -0,0 +1,46 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    fresh_jwt_required,
+    jwt_optional,
+    jwt_required,
+    get_jwt_claims,
+    get_jwt_identity,
+)
+from models.prescription import PrescriptionModel
+
+class PrescriptionRegister(Resource):
+    _prescription_parser = reqparse.RequestParser()
+    _prescription_parser.add_argument("text", type=str, required=True)
+    _prescription_parser.add_argument("patient_id", type=int, required=True)
+    _prescription_parser.add_argument("patient_name", type=str, required=True)
+    _prescription_parser.add_argument("doctor_name", type=str, required=True)
+
+    @jwt_required
+    def post(cls):
+        if get_jwt_claims()['type'] == 'doctor':
+            data = PrescriptionRegister._prescription_parser.parse_args()
+            prescription = PrescriptionModel(**data)
+            prescription.save_to_db()
+            return {'message': 'Prescription Added.'}
+        return {'message': 'Only a doctor can add prescriptions.'}, 401
+
+class Prescription(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls, prescription_id):
+        prescription = PrescriptionModel.find_by_id(prescription_id)
+        if prescription:
+            return prescription.json()
+        return {'message': 'prescription with that id does not exist'}, 404
+
+class PrescriptionList(Resource):
+    @classmethod
+    @jwt_required
+    def get (cls):
+        identity = get_jwt_identity()
+        prescriptions = PrescriptionModel.query.filter_by(patient_id=identity).all()
+        prescription_list = [prescription.json() for prescription in prescriptions]
+        return prescription_list, 200
+            
+
+
