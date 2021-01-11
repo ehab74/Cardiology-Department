cb2f424918dd6922fe5b959fc6f0f732acfaadc8
ehab74 <ehabwahba4@gmail.com>
2021-01-11 03:45:02 +0200

    analytics

---
+++ code/models/analytics.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+from models.patient import PatientModel
+from models.doctor import DoctorModel
+from models.appointment import AppointmentModel
+from sqlalchemy import func
+
+
+def find_count(date: datetime):
+
+    end_date = datetime.now().date()
+    analytics_list = []
+    while date <= end_date:
+        analytics_list.append(
+            {"date": date, "doctors": "0", "patients": "0", "appointments": "0"}
+        )
+        date += timedelta(days=1)
+
+    appointments = (
+        AppointmentModel.query.with_entities(
+            AppointmentModel.created_at, func.count(AppointmentModel.id)
+        )
+        .group_by(AppointmentModel.created_at)
+        .all()
+    )
+    doctors = (
+        DoctorModel.query.with_entities(
+            DoctorModel.created_at, func.count(DoctorModel.id)
+        )
+        .group_by(DoctorModel.created_at)
+        .all()
+    )
+    patients = (
+        PatientModel.query.with_entities(
+            PatientModel.created_at, func.count(PatientModel.id)
+        )
+        .group_by(PatientModel.created_at)
+        .all()
+    )
+
+    appointment = 0
+    doctor = 0
+    patient = 0
+    for x in analytics_list:
+        if x["date"] == appointments[appointment][0]:
+            x["appointments"] = appointments[appointment][1]
+            appointment += 1
+        if x["date"] == doctors[doctor][0]:
+            x["doctors"] = doctors[doctor][1]
+            doctor += 1
+        if x["date"] == patients[patient][0]:
+            x["patients"] = patients[patient][1]
+            patient += 1
+        x["date"] = x["date"].strftime("%Y-%m-%d")
+    return analytics_list
